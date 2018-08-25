5e817a79e3ea68592091dfcba8d67e1999641f41
Noah Portes Chaikin <noah@porteschaikin.com>
2018-08-25 18:51:51 -0400

    Add point radius to venue scatter
    Use distinct tagged users at each venue

--- app/models/queries/venues_scatter.rb
+++ app/models/queries/venues_scatter.rb
@@ -3,13 +3,20 @@ module Queries
     select
       venues.name,
       venues.rating,
-      count(*) check_ins_count
+      count(*) check_ins_count,
+      (
+        select count(distinct(check_in_tagged_users.user_id))
+        from check_ins ci1
+        join check_in_tagged_users
+          on check_in_tagged_users.check_in_id = ci1.id
+        where ci1.user_id = :user_id
+          and ci1.venue_id = venues.id
+      ) distinct_tagged_users_count
     from check_ins
     join venues
       on venues.id = check_ins.venue_id
       and venues.rating is not null
     where check_ins.user_id = :user_id
-      and venues.rating is not null
     group by venues.id
   SQL
 end
