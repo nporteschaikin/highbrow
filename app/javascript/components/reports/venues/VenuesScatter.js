4af5d538db474cbd4dbf684931268921d7f5ae05
Noah Portes Chaikin <noah@porteschaikin.com>
2018-08-26 09:46:09 -0400

    Clean up venues scatter code

---
+++ app/javascript/components/reports/venues/VenuesScatter.js
@@ -0,0 +1,97 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { Scatter } from "react-chartjs-2"
+
+class VenuesScatter extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isReady: false,
+    }
+  }
+
+  componentDidMount () {
+    fetch(this.props.path).
+      then(response => response.json()).
+      then((rows) => {
+        this.setState({
+          isReady: true,
+          rows: rows,
+          data: {
+            datasets: [{
+              data: rows.map((row) => { return { x: row.check_ins_count, y: row.rating } }),
+              pointRadius: rows.map(row => (row.distinct_tagged_users_count + 1) * 2),
+            }],
+          },
+        });
+      });
+  }
+
+  getData() {
+    const { rows } = this.state;
+
+    return {
+      datasets: [{
+        data: rows.map((row) => {
+          return {
+            x: row.check_ins_count,
+            y: row.rating,
+          }
+        }),
+        pointRadius: rows.map((row) => {
+          return (row.distinct_tagged_users_count + 1) * 2;
+        }),
+      }],
+    };
+  }
+
+  getOptions() {
+    const { rows } = this.state;
+
+    return {
+      tooltips: {
+        mode: 'single',
+        callbacks: {
+          title: (point) => {
+            var row = rows[point[0].index];
+            return row.name + " (" + row.rating + ")";
+          },
+          label: (point) => {
+            var row = rows[point.index];
+            return row.check_ins_count + " check-ins";
+          },
+        },
+      },
+      legend: {
+        display: false,
+      },
+      scales: {
+        xAxes: [{
+          display: false,
+        }],
+        yAxes: [{
+          display: false,
+        }],
+      },
+    };
+  }
+
+  render () {
+    if (this.state.isReady) {
+      return (
+        <Scatter data={this.getData()} options={this.getOptions()} />
+      );
+    }
+
+    return (
+      <p>Loading...</p>
+    );
+  }
+}
+
+VenuesScatter.propTypes = {
+  path: PropTypes.string
+};
+
+export default VenuesScatter
